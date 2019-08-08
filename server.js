7c67ba75949e53cfca9df387517a4261477bfaeb
Ryan Miller <ryanmillergm@gmail.com>
2019-08-08 13:49:52 -0600

    Adds inital server setup

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const DARK_SKY_API_KEY = process.env.DARK_SKY_API_KEY
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => {
+
+})
+
+app.listen(3000, () => {
+  console.log('Server Started')
+})
